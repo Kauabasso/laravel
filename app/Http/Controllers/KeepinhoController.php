8f633da40e9541c2e4386e78cdaa92055af57ad8
Kauabasso <kauabasso8@gmail.com>
2025-04-29 08:51:32 -0300

    aula 3

--- app/Http/Controllers/KeepinhoController.php
+++ app/Http/Controllers/KeepinhoController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nota;
 use Illuminate\Http\Request;
 
 class KeepinhoController extends Controller
 {
     public function index(){
-        return view('keepinho/index');
+        $notas = Nota::all();
+        //dd($notas);//importante para quando quiser entender o código(dados do banco, por exemplo)
+        return view('keepinho/index', [
+            'notas' => $notas,
+        ]);
+    }
+
+    public function gravar(Request $request){
+        dd($request);
     }
 }
--- resources/views/keepinho/index.blade.php
+++ resources/views/keepinho/index.blade.php
@@ -1,3 +1,17 @@
 <h1>Keepinho</h1>
 
 <p>Seja bem-vindo ao Keepinho, o seu assistente pessoal</p>
+<hr>
+<form action="{{ route('keep.gravar') }}" method="post">
+    @csrf
+    <textarea name="texto" cols="30" rows="10"></textarea>
+    <br>
+    <input type="submit" value="Gravar nota">
+</form>
+<hr>
+@foreach ($notas as $nota)
+    <div style="border:1px dashed green; padding: 2px">
+        {{ $nota->texto }}
+    </div>
+
+@endforeach
