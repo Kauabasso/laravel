8b028dce7d05bcde57f636c9539b30cf9f203f44
Kauabasso <kauabasso8@gmail.com>
2025-06-10 08:52:49 -0300

    aula 10/06

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -11,14 +11,32 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
                 <form method="POST" action="{{ route('produtos.store') }}">
-                @csrf
-                <div>
-            <x-input-label for="nome" :value="__('Nome')" />
-            <x-text-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-            </div>
-            
-            </form>
+                    @csrf
+                    <!--Nome-->
+                    <div>
+                        <x-input-label for="nome" :value="__('Nome')" />
+                        <x-text-input id="nome" class="block mt-1 " type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    
+                    <!--Preço-->
+                    <div>
+                        <x-input-label for="preco" :value="__('Preço')" />
+                        <x-text-input id="preco" class="block mt-1 " type="number" name="preco" :value="old('preco')" required autofocus autocomplete="preco" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                      <!--Descrição-->
+                      <div>
+                        <x-input-label for="descricao" :value="__('Descrição')" />
+                        <x-textarea id="descricao" class="block mt-1 " name="descricao" required autofocus autocomplete="descricao" >{{ old('descricao') }}</x-textarea>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    <br>
+                    <x-primary-button>
+                        Gravar Produto
+                    </x-primary-button>
+                </form>
                 </div>
             </div>
         </div>
